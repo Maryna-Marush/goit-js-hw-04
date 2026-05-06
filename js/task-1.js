181453b493ec7a5b4373f74d247acc1370d8245e
Maryna Marushchenko <marismarush@gmail.com>
2026-05-06 16:27:45 +0300

    add correction

--- js/task-1.js
+++ js/task-1.js
@@ -7,7 +7,6 @@ function isEnoughCapacity(products, containerSize) {
   return totalProducts <= containerSize;
 }
 
-// Перевірка коректності роботи:
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
 ); // true
